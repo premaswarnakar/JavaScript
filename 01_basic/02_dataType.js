63574feb9f4b9bef3448f5b1f277d7b46a812cc2
Prema Swarnakar <premaswarnakar1234@gmail.com>
2025-07-26 18:06:02 +0000

    DataType

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3); //we are using node js, not browser
+
+console.log(3 
+    + 3); // code readability should be high
+
+console.log("Prema");
+
+let name = "Prema";
+let age = 19;
+let isLoggeIn = false;
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/flase
+// null => standalone value
+// undifined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object 
