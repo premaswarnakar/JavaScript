a39ca00593c9cc422517ae75835cd0ed080125d4
Prema Swarnakar <premaswarnakar1234@gmail.com>
2025-07-27 21:10:23 +0000

    DataTypeConversion

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = 33;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Prema";
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
