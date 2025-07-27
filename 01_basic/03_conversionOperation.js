748660bffc6775633dce4848f4289faebcb5750b
Prema Swarnakar <premaswarnakar1234@gmail.com>
2025-07-27 21:24:41 +0000

    variable

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -12,6 +12,17 @@ let valueInNumber = number(score);
 // true => 1; false => 0
 
 let isLoggedIn = "Prema";
+
+let booleanIsLoggedIn = Boolean(isLLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Prema" => true
+
+let someNumber = 33;
+
+let stringNumber = string(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
